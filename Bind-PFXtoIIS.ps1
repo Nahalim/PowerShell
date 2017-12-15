85b2201f5eb072bb12efdbb2fdf3cb8715c974c8
Nahalim <33495094+Nahalim@users.noreply.github.com>
2017-12-15 15:52:43 -0500

    Initial Files

---
+++ Bind-PFXtoIIS.ps1
@@ -0,0 +1,74 @@
+﻿Function Bind-PFXtoIIS {
+    <#
+
+    .SYNOPSIS
+    Installs and Binds a PFX to an IIS Web Site
+
+    .DESCRIPTION
+    This script copys a PFX locally to C:\windows\temp. Then installs it and binds it in IIS. Then removes the file from the temp directory.
+
+    .EXAMPLE
+    Bind-PFXtoIIS -ComputerName Server01 -Certificate "C:\temp\myCert.pfx" -Password "Password1"
+
+    $servers = 1..10 | %{"HSWeb-WEB-$_"}
+    $servers | Bind-PFXtoIIS -Certificate "c:\temp\myCert.pfx" -Password "Password1"
+
+    .NOTES
+    Need to add CER support.
+
+    .LINK
+    https://github.com/Nahalim/PowerShell
+
+    #>
+
+    Param (
+        [CmdLetBinding()]
+        [Parameter(Mandatory=$true, Position=1, ValueFromPipeLine=$true)]
+        [string[]]$ComputerName,
+
+        [Parameter(Mandatory=$true)]
+        [ValidateScript({
+            Test-Path $_ 
+        })]
+        [string]$Certificate,
+        [Parameter(Mandatory=$true)]                
+        [string]$Password,
+        [string]$WebSite = "Default Web Site",
+        [string]$Port = 443
+    )
+
+
+    Begin {   
+    }
+
+    Process {
+        foreach ($PC in $ComputerName) {
+            Try {
+                Write-Verbose "Copying $Certificate to $PC"
+                Copy-Item -Path $Certificate -Destination "\\$PC\c$\Windows\Temp\bindiis.pfx" -Force
+                Invoke-Command -ComputerName $PC -ArgumentList $Password, $WebSite, $Port  {
+                    Import-Module WebAdministration
+                    Write-Verbose "    Importing Certificate to Computer Local Store"
+                    $import = Import-PfxCertificate -FilePath "c:\Windows\Temp\bindiis.pfx" -CertStoreLocation 'cert:\LocalMachine\MY' -Password (ConvertTo-SecureString $Args[0] -AsPlainText -force)            
+                    Write-Verbose "    Binding to IIS"
+                    New-WebBinding -Name $Args[1] -IP "*" -Port $Args[2] -Protocol https
+                    (Get-WebBinding -Name $Args[1] -Port $Args[2] -Protocol "https").AddSSlCertificate($import.Thumbprint,"My")
+                }
+
+                
+            }
+            catch {
+                Write-Host "$($_.Exception.Message)" -ForegroundColor Red
+            }
+            Finally {
+                Write-Verbose "    Removing cert from temp"
+                Remove-Item "\\$PC\c$\Windows\Temp\bindiis.pfx" -Force | Out-Null
+            }
+        }
+    }
+
+    End {
+
+    }
+
+}
---
+++ IIS-Administrations.ps1
@@ -0,0 +1,260 @@
+﻿Function Parse-IISLogs {
+    <#
+
+    .SYNOPSIS
+    View IIS logs on Remote Servers
+
+    .DESCRIPTION
+    A Primitive script that pulls IIS logs from a remote server and converts the time to local time.
+
+    .EXAMPLE
+    Parse-IISLogs -ComputerName "SERVER01" -StartDate (Get-Date).Adddays(-10)
+    Parse-IISLogs -ComputerName "SERVER01" -WithinHours 4 | Out-GridView
+
+    .NOTES
+    It's primitive but works?
+
+    .LINK
+    https://github.com/Nahalim/PowerShell
+
+    #>
+
+    Param (
+        [CmdLetBinding()]
+        [Parameter(Mandatory=$true, Position=1)]
+        [ValidateScript({
+            Test-Connection $_ -Quiet
+        })]
+        [string]$ComputerName,
+        [string]$LogPath = "C:\inetpub\logs\LogFiles",
+        [string]$StartDate = (Get-Date).AddDays(-1),
+        [string]$EndDate = (Get-Date),
+        [ValidateRange(0,24)]
+        [int]$WithinHours = 0
+    )
+    
+    If ($withinHours -gt 0) {
+        #No sense in parsing older logs if we only want X hours
+        $StartDate = (Get-Date).AddDays(-1)
+        $EndDate = (Get-Date)
+    }
+
+    $Logs = Get-ChildItem $LogPath.Replace("C:", "\\$ComputerName\c$") -Filter *.log -Recurse | Where-Object { $_.CreationTime -ge $StartDate -and $_.CreationTime -le $EndDate }
+    $IISData = @()
+
+    $i = 0
+    Write-Host "Reading $($Logs.Count) files..."
+    foreach ($log in $Logs) {
+        $i++; Write-Progress -Activity "Reading $($log.FullName)" -PercentComplete (($i / $logs.Count) * 100) -Id 0 
+        $tmp = Get-Content $log.FullName | % {
+            $tmpLine = $_
+            If (-not $tmpLine.StartsWith("#")) { $tmpLine } 
+        }
+    
+
+        $j = 0
+        Write-Host "$($log.FullName) contains $($tmp.count) items)"
+        foreach ($line in $tmp) {
+            Write-Progress -Activity "Parsing entires" -PercentComplete (($j / $tmp.Count) * 100) -Id 1;$j++ 
+            $tmpLSplit = $line.Split(" ")
+            
+            $realTime = ([datetime]::ParseExact("$($tmpLSplit[0]) $($tmpLSplit[1])", 'yyyy-MM-dd HH:mm:ss', $null))
+            $realTime =  [datetime]::SpecifyKind($realTime,'Utc').ToLocalTime()
+            $rObj = New-Object -TypeName PsObject
+            $rObj | Add-Member -MemberType NoteProperty -Name TimeStamp -Value $realTime
+            $rObj | Add-Member -MemberType NoteProperty -Name s_ip -Value $tmpLSplit[2]
+            $rObj | Add-Member -MemberType NoteProperty -Name cs_Method -Value $tmpLSplit[3]
+            $rObj | Add-Member -MemberType NoteProperty -Name cs_uri_stem -Value $tmpLSplit[4]
+            $rObj | Add-Member -MemberType NoteProperty -Name cs_uri_query -Value $tmpLSplit[5]
+            $rObj | Add-Member -MemberType NoteProperty -Name s_port -Value $tmpLSplit[6]
+            $rObj | Add-Member -MemberType NoteProperty -Name cs_username -Value $tmpLSplit[7]
+            $rObj | Add-Member -MemberType NoteProperty -Name c_ip -Value $tmpLSplit[8]
+            $rObj | Add-Member -MemberType NoteProperty -Name cs_User_Agent -Value $tmpLSplit[9]
+            $rObj | Add-Member -MemberType NoteProperty -Name cs_Referer -Value $tmpLSplit[10]
+            $rObj | Add-Member -MemberType NoteProperty -Name sc_status -Value $tmpLSplit[11]
+            $rObj | Add-Member -MemberType NoteProperty -Name sc_substatus -Value $tmpLSplit[12]
+            $rObj | Add-Member -MemberType NoteProperty -Name sc_win32_status -Value $tmpLSplit[13]
+            $rObj | Add-Member -MemberType NoteProperty -Name time_taken -Value $tmpLSplit[14]
+            $IISData += $rObj    
+        }
+    }
+    If ($withinHours -gt 0) {
+        $IISData | ?{$_.TimeStamp -gt (Get-Date).AddHours($WithinHours * -1)}
+    }
+    Else {
+        $IISData 
+    }
+}
+
+
+Function Get-AppPool{
+    <#
+
+    .SYNOPSIS
+    Returns Remote App Pools in IIS
+
+    .DESCRIPTION
+    This command will return application pools from IIS using Web.Administration.
+
+    .EXAMPLE
+    Get-AppPool -ComputerName Server01 -AppPool "*Default*"
+    Get-AppPool -ComputerName Server01, Server02 -AppPool "*Default*"
+    Get-AppPool -ComputerName Server01 -AppPool "App Pool 1"
+
+    .NOTES
+    I need to replace object check with if it's the actual object... sloppy
+
+    .LINK
+    https://github.com/Nahalim/PowerShell
+
+    #>
+
+    Param (
+        [CmdLetBinding()]
+        [Parameter(Mandatory=$true, Position=1, ValueFromPipeLine=$true)]
+        [string[]]$ComputerName,        
+        [string]$AppPool = "*"
+    )
+    Begin {
+        [Reflection.Assembly]::LoadWithPartialName('Microsoft.Web.Administration') | Out-Null
+    }
+    Process { 
+        Foreach ($PC in $ComputerName) {
+            Write-Host "Checking $PC"
+            Try {       
+                $sm = [Microsoft.Web.Administration.ServerManager]::OpenRemote("$PC")
+                $sm.ApplicationPools | ? {$_.Name -like $AppPool}
+            }
+            Catch {
+                $_.Exception.Message
+            }
+        }
+        
+    }
+}
+
+Function Stop-AppPool{
+    <#
+
+    .SYNOPSIS
+    Stops IIS App Pools
+
+    .DESCRIPTION
+    This command will Stop remote application pools. Requires Get-AppPool to function. 
+    You can pipe Get-AppPool into it or specify a search string to have it call out.
+
+    .EXAMPLE
+    Stop-AppPool -ComputerName Server01 -AppPool "*Default*"
+    Stop-AppPool -ComputerName Server01, Server02 -AppPool "*Default*"
+    Stop-AppPool -ComputerName Server01 -AppPool "App Pool 1"
+
+    Get-AppPool -ComputerName Server01 -AppPool "*Default*" | Stop-AppPool
+
+    .NOTES
+    
+
+    .LINK
+    https://github.com/Nahalim/PowerShell
+
+    #>
+
+    Param (
+        [CmdLetBinding()]
+        [Parameter(ValueFromPipeline)]        
+        $AppPool = "*",
+
+        [string[]]$ComputerName = "localhost"
+    )
+
+    Begin {
+        
+        [Reflection.Assembly]::LoadWithPartialName('Microsoft.Web.Administration') | Out-Null        
+    }
+
+    Process { 
+        If ($AppPool -is [string] -or $AppPool -is [String[]]) {
+            
+            foreach ($PC in $ComputerName) {
+                $tmpAppPool = Get-AppPool -ComputerName $PC -AppPool $AppPool                                    
+            }
+            foreach ($pool in $tmpAppPool) {
+                Write-Host "Stopping $($pool.name)" -ForegroundColor Red
+                $pool.Stop()   
+            }
+        }
+        else {
+            
+            $tmpAppPool = $AppPool
+            
+            foreach ($pool in $tmpAppPool) {
+                Write-Host "Stopping $($pool.name)" -ForegroundColor Red
+                $pool.Stop()                
+            }
+        }
+
+    }   
+}
+
+
+ Function Recycle-AppPool{
+    <#
+
+    .SYNOPSIS
+    Stops IIS App Pools
+
+    .DESCRIPTION
+    This command will Stop remote application pools. Requires Get-AppPool to function. 
+    You can pipe Get-AppPool into it or specify a search string to have it call out.
+
+    .EXAMPLE
+    Recycle-AppPool -ComputerName Server01 -AppPool "*Default*"
+    Recycle-AppPool -ComputerName Server01, Server02 -AppPool "*Default*"
+    Recycle-AppPool -ComputerName Server01 -AppPool "App Pool 1"
+
+    Get-AppPool -ComputerName Server01 -AppPool "*Default*" | Recycle-AppPool
+
+    .NOTES
+    
+
+    .LINK
+    https://github.com/Nahalim/PowerShell
+
+    #>
+
+    Param (
+        [CmdLetBinding()]
+        [Parameter(ValueFromPipeline)]        
+        $AppPool = "*",
+
+        #[Parameter(Mandatory=$true)]
+        [string[]]$ComputerName = "localhost"
+    )
+
+    Begin {
+        
+        [Reflection.Assembly]::LoadWithPartialName('Microsoft.Web.Administration') | Out-Null        
+    }
+
+    Process { 
+        If ($AppPool -is [string] -or $AppPool -is [String[]]) {
+            
+            foreach ($PC in $ComputerName) {
+                $tmpAppPool = Get-AppPool -ComputerName $PC -AppPool $AppPool                                    
+            }
+            foreach ($pool in $tmpAppPool) {
+                Write-Host "Stopping $($pool.name)" -ForegroundColor Red
+                $pool.recycle()   
+            }
+        }
+        else {
+            
+            $tmpAppPool = $AppPool
+            
+            foreach ($pool in $tmpAppPool) {
+                Write-Host "Stopping $($pool.name)" -ForegroundColor Red
+                $pool.recycle()                
+            }
+        }
+
+    }   
+}
